f15f418d202f916770ccae404b5b51dfa63e64ab
angeledesert <105069949+angeledesert@users.noreply.github.com>
2022-05-06 19:25:15 +0200

    Add files via upload

---
+++ creer_une_liste_de_liste.py
@@ -0,0 +1,13 @@
+﻿from random import *
+
+def creer_liste_de_liste(hauteur,largeur):
+    liste_alea=[]
+    liste_de_liste=[]
+    for j in range(4):
+        for i in range(3):
+            liste_alea.append(randint(0, 100))
+        liste_de_liste.append(liste_alea)
+        liste_alea=[]
+    return(liste_de_liste)
+mes_donnees=creer_liste_de_liste(4,3)
+print(mes_donnees)
